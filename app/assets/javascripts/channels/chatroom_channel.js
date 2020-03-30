b1e1d8f2b3e04f50026e4590ea541c9a5490b0bf
Neto Concon <concon.neto2@gmail.com>
2020-03-29 22:59:28 -0300

    add chatroom chanel

--- app/assets/javascripts/channels/chatroom_channel.js
+++ app/assets/javascripts/channels/chatroom_channel.js
@@ -6,7 +6,7 @@ if (messagesContainer) {
 
   consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
     received(data) {
-      console.log(data); // called when data is broadcast in the cable
+      messagesContainer.insertAdjacentHTML('beforeend', data);
     },
   });
 }
